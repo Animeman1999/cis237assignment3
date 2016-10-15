64a29e881f6ad6508c1c2fda7bc7c97285cef3df
Jeffrey Martin <animeman1999@gmail.com>
2016-10-15 18:38:51 -0400

    Adds IDroidCollection and DroidCollection classes

---
+++ cis237assignment3/DroidCollection.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237assignment3
+{
+    class DroidCollection : IDroidCollection
+    {
+        //***************************************
+        //Variables
+        //***************************************
+        Droid[] droidItems;
+        int droidItemsLengthInt;
+
+
+        //***************************************
+        //Method
+        //***************************************
+        public void AddNewItem() { }
+
+        public void AddNewItem(string MaterialString, string ModelString, string ColorString, int NumberLanguagesInt)
+        {
+            droidItems[droidItemsLengthInt] = new Protocol(MaterialString, ModelString, ColorString, NumberLanguagesInt);
+            droidItemsLengthInt++;
+        }
+
+        public void AddNewItem(string MaterialString, string ModelString, string ColorString, bool ToolboxBool, bool ComputerConnectionBool, bool ArmBool)
+        {
+            droidItems[droidItemsLengthInt] = new Utility(MaterialString, ModelString, ColorString, ToolboxBool, ComputerConnectionBool, ArmBool);
+            droidItemsLengthInt++;
+        }
+
+        public void AddNewItem (string MaterialString, string ModelString, string ColorString, bool ToolboxBool,
+            bool ComputerConnectionBool, bool ArmBool, bool TrashCompactorBool, bool VacuumBool)
+        {
+            droidItems[droidItemsLengthInt] = new Janitor(MaterialString, ModelString, ColorString, ToolboxBool,
+            ComputerConnectionBool, ArmBool, TrashCompactorBool, VacuumBool);
+            droidItemsLengthInt++;
+        }
+
+        public void AddNewItem(string MaterialString, string ModelString, string ColorString, bool ToolboxBool,
+            bool ComputerConnectionBool, bool ArmBool, bool FireExtinquisher, int NumberShips)
+        {
+            droidItems[droidItemsLengthInt] = new Astromech(MaterialString, ModelString, ColorString, ToolboxBool,
+            ComputerConnectionBool, ArmBool, FireExtinquisher, NumberShips);
+            droidItemsLengthInt++;
+        }
+
+
+        //***************************************
+        //Constructor
+        //***************************************
+
+        DroidCollection (int Size)
+        {
+            droidItems = new Droid[Size];
+            droidItemsLengthInt = 0;
+        }
+
+    }
+}
---
+++ cis237assignment3/IDroidCollection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237assignment3
+{
+    interface IDroidCollection
+    {
+        void AddNewItem();
+
+    }
+}
