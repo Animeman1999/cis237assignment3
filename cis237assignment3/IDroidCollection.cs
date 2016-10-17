1523863e24cea8cd57fc527bfb09be5cc9d0a5a3
Jeffrey Martin <animeman1999@gmail.com>
2016-10-17 15:37:00 -0400

    Adds comments to IDroidCollections and Program collections

--- cis237assignment3/IDroidCollection.cs
+++ cis237assignment3/IDroidCollection.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 
 namespace cis237assignment3
 {
+    /// <summary>
+    /// Make sure that the DroidCollection will have 4 overloaded methods for AddNewItem
+    /// </summary>
     interface IDroidCollection
     {
         void AddNewItem(string MaterialString, string ModelString, string ColorString, int NumberLanguagesInt);
--- cis237assignment3/Program.cs
+++ cis237assignment3/Program.cs
@@ -13,22 +13,30 @@ namespace cis237assignment3
     {
         static void Main(string[] args)
         {
+            //***************************************
+            //Variables
+            //***************************************
+
             string materialTest = "plastic";
             string DroidTypeTest = "Protocol"; 
             string colorTest = "red";
             int numberLanguagesTest = 1000;
             int menuChoice;
+            const int DROID_COLLECTION_SIZE = 1000; 
+            
+            //Create a single DroidCollection to be used for the entire program
+            DroidCollection droidCollection = new DroidCollection(DROID_COLLECTION_SIZE);
 
-            string[] outputString;
-
-            DroidCollection droidCollection = new DroidCollection(1000);
-
+            //Create a single UserInterface to be used for the entire program
             UserInterface ui = new UserInterface();
 
+            //Create the console to be used
             ui.StartUserInterface();
 
+            //Create the LoadMenu and load the user choice
             menuChoice = ui.LoadMenu();
 
+            // Either user wants to load a droid list or  not
             switch (menuChoice)
             {
                 case 1:             
@@ -71,18 +79,20 @@ namespace cis237assignment3
                     break;  
             }
 
+            //Continue to loop until the user chooses 4 which is to exit.
             while (menuChoice != 4)
             {
                 menuChoice = ui.MainMenu();
                 switch (menuChoice)
                 {
-                    case 1:
+                    case 1://Print out the droid list
                         ui.PrintDroidList(droidCollection.GetListOfAllDroids());
                         break;
-                    case 2:
+                    case 2://Add a new droid to the DroidCollection
                         ui.AddDroidSequence(droidCollection);
                         break;
-                    case 3:
+                    case 3://Delete a droid from the DroidCollection
+                                //Make sure the DroidCollection has Droids in them
                         if(droidCollection.NumberOfDroidsInList > 0)
                         {
                             ui.PrintDroidList(droidCollection.GetListOfAllDroids());
@@ -94,14 +104,11 @@ namespace cis237assignment3
                         }
                         
                         break;
-                    default:
+                    default://Exit the program
                         ui.ExitMessage();
                         break;
                 }
-
             }
         }
- 
-
     }
 }
